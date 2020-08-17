662522104f442ec0e2e8394734a146905a560da4
404kevnotfound <student@iMac.local>
2020-08-17 13:16:00 -0600

    add chapter27

---
+++ Chapter27/Exercise21_11/Exercise27_11.java
@@ -0,0 +1,30 @@
+// By: Travis Dowd
+// Date: 8-31-2020
+//
+// Chapter 27, exercise 11
+
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashSet;
+
+public class Exercise27_11 {
+  public static void main(String[] args) {
+    Set<String> set = new HashSet<String>(); 
+    set.add("Smith");
+    set.add("Anderson");
+    set.add("Lewis");
+    set.add("Cook"); 
+    set.add("Smith");
+  
+    ArrayList<String> list = setToList(set);
+    System.out.println(list);
+  }
+  
+  // Second "Smith" not added in set or arraylist
+  public static <E> ArrayList<E> setToList(Set<E> s) {
+    ArrayList<E> list = new ArrayList<>();  // Create ArrayList
+    for ( E e : s ) {                       // foreach loop for generic set
+      list.add( e );                        // add e to ArrayList
+    } return list;                          // return ArrayList
+  }
+}
---
+++ Chapter27/Exercise27_9/Exercise27_09.java
@@ -0,0 +1,27 @@
+/* 
+Author: Travis Dowd
+Date: 8-31-2020
+
+Given: "4.5"      Expected: 51451
+Given: "Hello"    Expected: 69609650
+*/
+
+import java.lang.Math;
+
+public class Exercise27_09 { 
+  public static void main(String[] args) {
+    String s = "4.5";
+    System.out.println(hashCodeForString(s));
+    s = "Hello";
+    System.out.println(hashCodeForString(s));
+  }
+  
+  public static int hashCodeForString( String s ) {
+    final int b = 31;
+    int hash = 0;
+    char[] arr = s.toCharArray();
+    for( int i = 0; i < s.length(); i++ ){
+      hash = b * hash + s.charAt( i );
+    } return hash;
+  }
+}
